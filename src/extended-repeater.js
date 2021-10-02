4da8a8675bdbd8b5d4d75771af3c3f998d521333
karlsonru <ivpolovnikov@hotmail.com>
2021-10-02 15:20:39 +0300

    fix: extended repeater solution improved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -23,7 +23,8 @@ export default function repeater(str,
                                   additionRepeatTimes,
                                   additionSeparator,
                                 }) {
-  let [repeatTimes, separator, addition, additionRepeatTimes, additionSeparator] = [(options.repeatTimes || 1), (options.separator || '+'), (options.addition || ''), (options.additionRepeatTimes || 1), (options.additionSeparator || '|')];
+  //let [repeatTimes, separator, addition, additionRepeatTimes, additionSeparator] = [(options.repeatTimes || 1), (options.separator || '+'), (options.addition || ''), (options.additionRepeatTimes || 1), (options.additionSeparator || '|')];
+  let {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;                             
 
   let strWithAddition = str + (additionRepeatTimes > 1 
     ? (addition + additionSeparator)
