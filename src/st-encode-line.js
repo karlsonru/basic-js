e83bac639a801eb0f7682a550f3e0c2eb8d7bc39
karlsonru <ivpolovnikov@hotmail.com>
2021-10-04 00:02:50 +0300

    feat: add solition encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let newStr = '';
+  let counter = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (i == str.length - 1) {
+      if (str[i-1] == str[i]) {
+        counter++;
+        newStr += counter + str[i];
+      } else {
+        newStr += counter > 1 ? counter + str[i-1] + str[i] : str[i-1] + str[i];
+      }
+      break; 
+    }
+
+    if (str[i-1] == str[i]) {
+      counter++;
+      continue;
+    }
+    
+    // если char at str[i] != char at str[i], то добавляем накопившуюся строку и переводим счётчик на единицу 
+    newStr += counter > 1 ? counter + str[i-1] : str[i-1];
+    counter = 1;
+  }
+  return newStr; 
 }
